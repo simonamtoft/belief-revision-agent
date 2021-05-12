0852de40b386b36a7d7d55b25eb85104694b4d60
MarcMarabou <17163097+MarcMarabou@users.noreply.github.com>
2021-05-12 19:13:36 +0200

    Revision and Wobcke test

--- BeliefBase.py
+++ BeliefBase.py
@@ -178,15 +178,16 @@ class BeliefBase:
         return 'BeliefBase([\n  {}\n])'.format(",\n  ".join(str(x) for x in self.beliefs))
 
     def rank(self, formula):
+        formula = to_cnf(formula)
         bb = true
-        r = self.beliefs[0].order
+        r = self.beliefs[0].order if self.beliefs else 0
         for belief in self.beliefs:
             if belief.order < r:
-                if entails(to_cnf(bb), formula):
+                if entails(bb, formula):
                     return r
                 r = belief.order
-            bb = bb & belief.formula
-        return r if entails(to_cnf(bb), formula) else 0
+            bb = bb & to_cnf(belief.formula)
+        return r if entails(bb, formula) else 0
     
     def expand(self, formula, newrank):
         if self.rank(Not(formula)) > 0:
@@ -200,36 +201,42 @@ class BeliefBase:
             if formula == belief.formula:
                 self.beliefs.remove(belief)
         self.beliefs.add(Belief(formula, newrank))
+        print(f">>> {formula} added to belief basis with rank {newrank}")
         for belief in self.beliefs:
             if oldrank <= belief.order <= newrank:
-                bb = [x.formula for x in filter(lambda x: x.order >= belief.order and x != belief, self.beliefs)]
-                bb = to_cnf(reduce(lambda x, y: x & y, bb, true))
+                bb = [to_cnf(x.formula) for x in filter(lambda x: x.order >= belief.order and x != belief, self.beliefs)]
+                bb = reduce(lambda x, y: x & y, bb, true)
                 if entails(bb, belief.formula):
-                    print(f">>> {belief} is redundant")
+                    print(f">>> Removed {belief} as it is redundant")
                     self.beliefs.remove(belief)
     
     def contract(self, formula):
         if entails(true, formula):
-            print(f"{formula} is a tautology")
+            print(f">>> {formula} is a tautology")
             return
         oldrank = self.rank(formula)
         delta = BeliefBase()
         delta.beliefs = self.beliefs.copy()
         for belief in self.beliefs:
             if belief.order <= oldrank:
-                bb = [x.formula for x in filter(lambda x: x.order >= (oldrank + 1), delta.beliefs)]
-                bb = to_cnf(reduce(lambda x, y: x & y, bb, true))
+                bb = [to_cnf(x.formula) for x in filter(lambda x: x.order >= (oldrank + 1), delta.beliefs)]
+                bb = reduce(lambda x, y: x & y, bb, true)
                 if not entails(bb, formula | belief.formula):
                     r = delta.rank(belief.formula)
                     delta.beliefs.remove(belief)
+                    print(f">>> {belief} removed by (C-) condition")
                     if r < oldrank or not entails(bb, formula >> belief.formula):
                         for b in self.beliefs:
                             if formula >> belief.formula == b.formula:
                                 delta.beliefs.remove(b)
-                        delta.beliefs.add(Belief(formula >> belief.formula, r))
+                        t = Belief(formula >> belief.formula, r)
+                        delta.beliefs.add(t)
+                        print(f">>> Added {t} to belief basis to satisfy (K-5)")
         self.beliefs = delta.beliefs
 
-
+    def revision(self, formula, newrank):
+        self.contract(Not(formula))
+        self.expand(formula, newrank)
             
 
 
@@ -249,12 +256,29 @@ EMPTY_BB = BeliefBase()
 
 
 if __name__ == "__main__":
+    pl = sp.parse_expr
     bb = BeliefBase()
-    bb.beliefs.add(Belief(sp.parse_expr("p"),    1))
-    bb.beliefs.add(Belief(sp.parse_expr("q"),    1))
-    bb.beliefs.add(Belief(sp.parse_expr("p>>q"), 1))
+    # bb.beliefs.add(Belief(sp.parse_expr("p"),    1))
+    # bb.beliefs.add(Belief(sp.parse_expr("q"),    1))
+    # bb.beliefs.add(Belief(sp.parse_expr("p>>q"), 1))
 
-    bb.contract(sp.parse_expr("~(q>>p)"))
-    print(bb)
-    bb.expand(sp.parse_expr("q>>p"), 1)
+    # bb.contract(sp.parse_expr("~(q>>p)"))
+    # print(bb)
+    # bb.expand(sp.parse_expr("q>>p"), 1)
+    # print(bb)
+
+    #  Example from Wobcke paper
+    bb.expand(pl("(p & q) >> r"), 100)
+    bb.expand(pl("r"), 10)
+    bb.expand(pl("p"), 20)
+    bb.expand(pl("q"), 30)
+    bb.expand(pl("p|r"), 40)
+    assert bb.rank(pl("r")) == 30
+    #print(bb)
+
+    # Contract r
+    bb.contract(pl("r"))
+    #print(bb)
+
+    bb.revision(pl("~r"), 40)
     print(bb)
