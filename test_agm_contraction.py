f9cb157d808903112a53b8b48baa03b54467a7a2
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-05-16 12:51:47 +0200

    update tests

--- test_agm_contraction.py
+++ test_agm_contraction.py
@@ -1,5 +1,6 @@
 from BeliefBase import BeliefBase
 import sympy as sp
+from sympy.logic.boolalg import And, Not
 
 # add a shorthand to use the sympy expression parser
 pl = sp.parse_expr
@@ -16,20 +17,46 @@ if __name__ == "__main__":
     
     print("\nAGM Postulates of Contraction:")
     
-    # success + inclusion
+    # closure, success + inclusion
+    print("\n=== CLOSURE, SUCCESS & INCLUSION ===")
     print(f'\ncontract with {phi}')
     bb.contract(phi)
     print(bb)
     
     # vacuity
+    print("\n=== VACUITY ===")
     print(f'\ncontract with {phi}')
     bb.contract(phi)
     print(bb)
     
     # recovery
+    print("\n=== RECOVERY ===")
     print(f'\nexpand with {phi}')
-    bb.expand(phi, 10)
+    bb.expand(phi, 20)
     print(bb)
 
-    
+    # conjunctive inclusion
+    print("\n=== CONJUNCTIVE INCLUSION / OVERLAP ===")
+    psi = pl('p')
+    tmp = bb.beliefs.copy()
+
+    print(f'\ncontract with "{phi} & {psi}"')
+    bb.contract(And(phi, psi))
+    print(bb)
+
+    print("\nrecover previous BB")
+    bb.beliefs = tmp.copy()
+    # print(bb)
+
+    print(f"\ncontract with {phi}")
+    bb.contract(phi)
+    print(bb)
+
+    # conjunctive overlap
+    print("recover BB again")
+    bb.beliefs = tmp.copy()
+    # print(bb)
+    print(f'\ncontract with {psi}')
+    bb.contract(psi)
+    print(bb)
 
--- test_agm_revision.py
+++ test_agm_revision.py
@@ -1,5 +1,7 @@
 from BeliefBase import BeliefBase
 import sympy as sp
+from sympy.logic.boolalg import And, Not
+
 
 # add a shorthand to use the sympy expression parser
 pl = sp.parse_expr
@@ -9,7 +11,7 @@ if __name__ == "__main__":
     bb = BeliefBase()
     phi = pl('k')
     bb.instantiate()
-
+    tmp = bb.beliefs.copy()
     print("\nInitial Belief Base:")
     print(bb)
     print(f'phi = {phi}')
@@ -17,14 +19,46 @@ if __name__ == "__main__":
     print("\nAGM Postulates of Revision:")
 
     # closure, success & vacuity
+    print("\n=== CLOSURE, SUCCESS & VACUITY ===")
     print(f'\nrevise with {phi}')
-    bb.revision(phi, 30)
+    bb.revision(phi, 5)
+    print(bb)
+
+    # Subexpansion
+    print("\n=== SUBEXPANSION ===")
+    phi_2 = pl('z')
+    print(f'\nexpand with {phi_2}')
+    bb.expand(phi_2, 5)
+    print(bb)
+    
+    bb.beliefs = tmp
+    print(f'\nrevise with with "{phi_2} & {phi}"')
+    bb.revision(And(phi_2, phi), 5)
     print(bb)
 
     # inclusion
-    print(f'\rcontract with {phi}, then expand with {phi}')
+    print("\n=== INCLUSION ===")
+    print(f'\nremove {phi} from previous BB, then expand with {phi}')
     bb.contract(phi)
-    bb.expand(phi, 30)
+    bb.expand(phi, 5)
+    print(bb)
+
+    # super expansion
+    print("\n=== SUPEREXPANSION ===")
+    phi_1 = pl('~p')
+    psi_1 = pl('~r')
+    tmp = bb.beliefs.copy()
+    print(f'\nrevise with "{phi_1} & {psi_1}"')
+    bb.revision(And(phi_1, psi_1), 20)
+    print(bb)
+
+    print("\nRecover previous BB.")
+    bb.beliefs = tmp
+    print(bb)
+
+    print(f"\nRevise with {phi_1}, expand with {psi_1}")
+    bb.revision(phi_1, 20)
+    bb.expand(psi_1, 20)
     print(bb)
 
 
